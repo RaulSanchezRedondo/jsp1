a08ad0a5eff9edf772b6dcf575a77d21f4933e8c
raul <raulsanchezredondo@hotmail.com>
2017-03-22 10:05:34 +0100

    Apliacion06

---
+++ src/java/util/HTMLFilter.java
@@ -0,0 +1,45 @@
+
+package util;
+
+/**
+ * HTML filter utility.
+ *
+ * @author Craig R. McClanahan
+ * @author Tim Tye
+ * @version $Id: HTMLFilter.java 939541 2010-04-30 01:43:56Z kkolinko $
+ */
+
+public final class HTMLFilter {
+
+    public static String filter(String message) {
+
+        if (message == null)
+            return (null);
+
+        char content[] = new char[message.length()];
+        message.getChars(0, message.length(), content, 0);
+        StringBuffer result = new StringBuffer(content.length + 50);
+        for (int i = 0; i < content.length; i++) {
+            switch (content[i]) {
+            case '<':
+                result.append("&lt;");
+                break;
+            case '>':
+                result.append("&gt;");
+                break;
+            case '&':
+                result.append("&amp;");
+                break;
+            case '"':
+                result.append("&quot;");
+                break;
+            default:
+                result.append(content[i]);
+            }
+        }
+        return (result.toString());
+
+    }
+
+
+}
